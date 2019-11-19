2a7bf5bebac91a56dd3ba97d79700c42ccd48992
Marek Pokropiński <marox900@gmail.com>
2019-11-19 10:31:58 +0100

    remove commented code

--- api/measurements_api/urls.py
+++ api/measurements_api/urls.py
@@ -21,9 +21,6 @@ from django.urls import path, include
 from django.conf.urls import url
 from rest_framework_swagger.views import get_swagger_view
 
-# schema_view = get_swagger_view(
-#     title="Medical Measurements API")
-
 
 @api_view()
 @renderer_classes([OpenAPIRenderer])
